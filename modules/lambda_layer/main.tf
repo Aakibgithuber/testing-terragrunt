440469b0dfa07f72c5e0e11b1ecad88129a707c2
Aakib khan <101191261+Aakibgithuber@users.noreply.github.com>
2025-11-03 11:24:19 +0530

    added new layer version

--- modules/lambda_layer/main.tf
+++ modules/lambda_layer/main.tf
@@ -13,4 +13,5 @@ resource "aws_lambda_layer_version" "this" {
   s3_bucket     = var.s3_bucket
   s3_key        = var.s3_key
   compatible_runtimes = var.compatible_runtimes
+  source_code_hash = timestamp()
 }
